667463fe1d21eaba8d1452c3b5478fbcf0db25f8
AkiSato <sato.geijo.13@gmail.com>
2016-12-01 18:13:38 +0900

    init

---
+++ Assets/Scripts/AutoGetComponent.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AutoGetComponent<T> where T :Component{
+
+	private T _instance;
+	public T Instance(MonoBehaviour behaviour)
+	{ 
+		if (_instance == null)
+		{
+			_instance = behaviour.GetComponent<T>();
+		}
+		return _instance;
+	}
+
+
+
+}
